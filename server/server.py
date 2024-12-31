5ce98139bdfc74818c506c0be16b8bb61641d53f
Szonni <SZonny@proton.me>
2024-12-31 14:21:45 +0700

    replaced flask with socket

--- server/server.py
+++ server/server.py
@@ -1,46 +1,35 @@
-from flask import Flask, request, send_from_directory, jsonify
-import os
+import socket
+import threading
 
-app = Flask(__name__)
+HEADER = 64 # Number of bytes to specify the message actual length
+PORT = 5000
+HOST = socket.gethostbyname(socket.gethostname()) # Get the IP address of the local computer
+ADDR = (HOST, PORT)
 
-# Directory to store files
-UPLOAD_FOLDER = 'server/uploaded_files'
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def awake():
+    print("THE SERVER IS AWAKENING...")
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr)) # Thread so not to block other clients while connected
+        thread.start()
 
-# Route for uploading files
-@app.route('/upload', methods=['POST'])
-def upload_files():
-    if 'files' not in request.files:
-        return jsonify({'error': 'No files part'}), 400
-    
-    files = request.files.getlist('files')  # Get list of files
-    if not files or all(file.filename == '' for file in files):
-        return jsonify({'error': 'No selected files'}), 400
-    
-    uploaded_files = []
-    for file in files:
-        if file and file.filename:  # Ensure file has a name
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(file_path)
-            uploaded_files.append(file.filename)
-    
-    return jsonify({'message': 'Files uploaded successfully!', 'files': uploaded_files})
+def handle_client(conn, addr):
+    print(f"NEW CLIENT CONNECTION: {addr}")
+    connected = True
+    while connected:
+        msg_length = conn.recv(HEADER).decode()
+        if not msg_length:
+            continue
+        msg_length = int(msg_length)
+        msg = conn.recv(msg_length).decode()
+        if msg == "quit":
+            connected = False
+        print(msg)
+    conn.close()
 
-
-# Route for downloading files
-@app.route('/download/<filename>', methods=['GET'])
-def download_file(filename):
-    try:
-        return send_from_directory(app.config['UPLOAD_FOLDER'], filename, as_attachment=True)
-    except FileNotFoundError:
-        return jsonify({'error': 'File not found'}), 404
-
-# List all files on server at home
-@app.route('/')
-def show_files():
-    files = [file.name for file in os.scandir(app.config['UPLOAD_FOLDER'])]
-    return files
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Set to be reusable socket bc there is a TIME_WAIT before you can use the socket again
+    awake()
